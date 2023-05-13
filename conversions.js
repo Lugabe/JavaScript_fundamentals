9a5010b774466ac73fc72bbf978ad41e05fb457b
Lugabe <luis2014gbp@gmail.com>
2023-05-13 18:28:53 -0300

    adicionando ;

--- conversions.js
+++ conversions.js
@@ -2,10 +2,10 @@
 //booleanos
 
 // conversão implícita
-const number = 500
-const numberString = " 500";
-console.log(number === numberString);
+const number =500;
+const numberString =" 500";
 
+console.log(number === numberString);
 //Number()
 //String()
 console.log(number + Number(numberString));
